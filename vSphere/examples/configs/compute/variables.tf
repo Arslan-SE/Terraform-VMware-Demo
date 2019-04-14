f4df752344b6ff8b531e19c13d0ff02b075cbe11
Arslan Saeed <arslan@hashicorp.com>
2019-04-14 15:38:41 -0400

    updated variables file.

--- vSphere/examples/configs/compute/variables.tf
+++ vSphere/examples/configs/compute/variables.tf
@@ -4,3 +4,10 @@ variable "datastore" {}
 variable "network" {}
 variable "resource_pool" {}
 variable "vm_name_prefix" {}
+variable "disk_size" {}
+
+variable "guest_id" {}
+variable "memory" {}
+variable "vm_count" {}
+variable "wait_for_guest_net_timeout" {}
+
