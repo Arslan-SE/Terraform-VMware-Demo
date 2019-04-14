850de1144b1f132c83e1878360095f8bf2d686bc
Arslan Saeed <arslan@hashicorp.com>
2019-04-14 05:16:55 -0400

    updates vars references in main.tf

--- vSphere/examples/configs/network/main.tf
+++ vSphere/examples/configs/network/main.tf
@@ -3,27 +3,27 @@ terraform {
   required_version = "= 0.11.13"
 }
 data "vsphere_datacenter" "datacenter" {
-  name = "${var.dc}"
+  name = "${var.Datacenter_Name}"
 }
 data "vsphere_host" "esxi_host" {
-  name          = "${var.host}"
+  name          = "${var.Host_Name}"
   datacenter_id = "${data.vsphere_datacenter.datacenter.id}"
 }
 # Management Network Portgroup
 resource "vsphere_host_port_group" "mgmt" {
   name                = "DEMO-MGMT_Network"
   host_system_id      = "${data.vsphere_host.esxi_host.id}"
-  virtual_switch_name = "${var.vswitch_name}"
+  virtual_switch_name = "${var.vSwitch_Name}"
 }
 # vMotion Network Portgroup
 resource "vsphere_host_port_group" "vmotion" {
   name                = "DEMO-vMotion Network"
   host_system_id      = "${data.vsphere_host.esxi_host.id}"
-  virtual_switch_name = "${var.vswitch_name}"
+  virtual_switch_name = "${var.vSwitch_Name}"
 }
 # VM Network Portgroup
 resource "vsphere_host_port_group" "vm" {
   name                = "DEMO-VM_Network"
   host_system_id      = "${data.vsphere_host.esxi_host.id}"
-  virtual_switch_name = "${var.vswitch_name}"
+  virtual_switch_name = "${var.vSwitch_Name}"
 }
