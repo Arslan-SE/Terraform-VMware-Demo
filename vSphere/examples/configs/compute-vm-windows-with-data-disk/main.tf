eb82e294dbea595c18d8ae567d404847fedf8cab
Arslan-SE <39010565+Arslan-SE@users.noreply.github.com>
2019-04-18 15:14:34 -0400

    Update main.tf

--- vSphere/examples/configs/compute-vm-windows-with-data-disk/main.tf
+++ vSphere/examples/configs/compute-vm-windows-with-data-disk/main.tf
@@ -14,7 +14,7 @@ module "vm" {
   ipv4submask = "255.255.255.0"
   is_windows_image = "true"
   ram_size = 8
-  vlan = "vSwitch0"
+  vlan = "VM Network"
   vmdns = ["8.8.8.8"]
   vmfolder = "TopGolf-Demo"
   vmgateway = "10.100.0.1"
