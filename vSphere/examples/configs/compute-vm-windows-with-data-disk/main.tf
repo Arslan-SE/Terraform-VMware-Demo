a9980cca24631942ce6efa018204c10dbd768b82
Arslan Saeed <arslan@hashicorp.com>
2019-04-18 15:00:36 -0400

    Add windows test config.

---
+++ vSphere/examples/configs/compute-vm-windows-with-data-disk/main.tf
@@ -0,0 +1,25 @@
+//--------------------------------------------------------------------
+// Modules
+module "vm" {
+  source  = "ptfe.this-demo.rocks/VMware-Demo/vm/vsphere"
+  version = "0.9.14"
+
+  cpu_number = 4
+  data_disk = "true"
+  data_disk_size_gb = 20
+  dc = "PacketDatacenter"
+  ds_cluster = "DatastoreCluster"
+  instances = 1
+  ipaddress = "10.100.0.223"
+  ipv4submask = "255.255.255.0"
+  is_windows_image = "true"
+  ram_size = 8
+  vlan = "vSwitch0"
+  vmdns = "8.8.8.8"
+  vmfolder = "TopGolf-Demo"
+  vmgateway = "10.100.0.1"
+  vmname = "Windows-Test"
+  vmrp = "MainCluster/Resources"
+  vmtemp = "win2012"
+  winadminpass = "Hashi123"
+}
