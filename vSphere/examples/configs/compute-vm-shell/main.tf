543905be1b28216379ead8a7def7a1810f5d06d7
Arslan-SE <39010565+Arslan-SE@users.noreply.github.com>
2019-05-02 09:38:27 -0400

    Update main.tf
    Removed IP Address output.

--- vSphere/examples/configs/compute-vm-shell/main.tf
+++ vSphere/examples/configs/compute-vm-shell/main.tf
@@ -23,7 +23,3 @@ output "virtual_machine_names" {
 output "virtual_machine_ids" {
   value = "${module.virtual_machines.virtual_machine_ids}"
 }
-
-output "virtual_machine_default_ips" {
-  value = "${module.virtual_machines.virtual_machine_default_ips}"
-}
